cbb69a36373d8c90014f3a1184dd5c39eb2452f6
raonixos_u0 <raonixos_u0@nixos.com>
2025-03-28 21:00:25 +0000

    with zsh plugins declarative

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@
   home.stateVersion = "24.05"; # Please read the comment before changing.
   home.packages = with pkgs; [
       libreoffice-qt
+      imagemagick
     ];
   home.file = {
   };
@@ -15,7 +16,48 @@
   };
   programs.home-manager.enable = true;
   #-----------------------------------------------------
-    programs.vim = {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      edit = "sudo -e";
+      update = "sudo nixos-rebuild switch";
+    };
+
+    history.size = 10000;
+    history.ignoreAllDups = true;
+    history.path = "$HOME/.zsh_history";
+    history.ignorePatterns = ["rm *" "pkill *" "cp *"];
+
+     plugins = [
+    {
+      name = "zsh-autocomplete";
+      src = pkgs.fetchFromGitHub {
+        owner = "marlonrichert";
+        repo = "zsh-autocomplete";
+        rev = "23.07.13";
+        sha256 = "sha256-/6V6IHwB5p0GT1u5SAiUa20LjFDSrMo731jFBq/bnpw=";
+     };
+     }
+
+     {
+      name = "zsh-syntax-highlighting";
+      src = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-syntax-highlighting";
+        rev = "0.8.0";
+        sha256 = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
+      };
+    }
+
+     ];
+     };
+
+      programs.vim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [ ];
     settings = { ignorecase = true; };
